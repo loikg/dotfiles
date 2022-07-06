9908da1f46ce95843f807c40944de9bcfecfe696
loikg <loikg@DESKTOP-2173576.localdomain>
2022-07-06 14:50:40 +0800

    feat: run ssh-agent

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Run ssh-agent
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  eval (ssh-add ~/.ssh/id_github)
+end

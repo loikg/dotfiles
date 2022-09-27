650a7e288d228785a9da8d39056d9c5c1b23ca06
loikg <loikg@mail.com>
2022-09-27 20:45:38 +0800

    feat: add editor, pnpm and rust env vars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,3 +12,14 @@ if test -z (pgrep ssh-agent)
 end
 
 export AWS_PROFILE=epitech
+
+# Rust
+fish_add_path "$HOME/.cargo/bin"
+
+# pnpm
+set -gx PNPM_HOME "/home/loik/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+# set editor
+set -gx EDITOR nvim

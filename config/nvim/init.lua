47294fcdaec25772f1dae712d0400730ded4129f
loikg <mail@company.com>
2023-03-15 15:05:34 +0800

    feat: add neovim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,27 @@
+-- enable numbers and relative numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- config/nvim/init.vim
+++
@@ -1,5 +0,0 @@
-" use Unicode
-set encoding=utf-8
-
-" show line number
-set number 

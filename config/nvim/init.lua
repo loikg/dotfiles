8ebbcac9f0abccbd9f2fc44c57473db04822412d
loikg <mail@company.com>
2023-11-24 10:15:41 +0800

    nvim: add nvim-tree

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -80,6 +80,40 @@ require("lazy").setup({
             },
         },
     },
+
+    {"nvim-tree/nvim-tree.lua"},
+})
+
+-- disable netrw as it's replace by nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local function nvim_tree_on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '<leader>b', api.tree.toggle)
+end
+
+-- nvim-tree
+require("nvim-tree").setup({
+    on_attach = nvim_tree_on_attach,
+	renderer = {
+		icons = {
+			show = {
+				file = false,
+				folder = false,
+				folder_arrow = false,
+				git = false,
+				modified = false,
+				diagnostics = false,
+				bookmarks = false,
+			}
+		}
+	}
 })
 
 -- LSP config

2714bf115129a05359a8b53a6e5decbaeb1f2e0c
loikg <mail@company.com>
2023-12-20 12:53:59 +0800

    nvim: add telescope find reference mapping

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -202,6 +202,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fm', builtin.marks, {})
 vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 -- telescope fzf
 require('telescope').load_extension('fzf')
 

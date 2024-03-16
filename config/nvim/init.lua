62626b187c78477137f99e82581091f822b2bd26
loikg <mail@company.com>
2024-03-16 12:15:26 +0800

    refactor: use config function

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,6 +12,50 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- disable netrw as it's replace by nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
+
+
+
+-- disable line wrapping
+vim.o.wrap = false
+
+vim.o.termguicolors = true
+
+-- Decrease update time
+vim.o.updatetime = 50
+
+-- enable numbers and relative numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+
+vim.g.mapleader = " "
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 require("lazy").setup({
     {
         "catppuccin/nvim",
@@ -57,8 +101,36 @@ require("lazy").setup({
     -- Telescope
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            -- telescope
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+            vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
+            vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+            local telescope = require('telescope')
+
+            telescope.setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
+                    }
+                },
+            }
+
+            telescope.load_extension("ui-select")
+            telescope.load_extension('fzf')
+        end
     },
     {
         'nvim-telescope/telescope-fzf-native.nvim',
@@ -66,17 +138,108 @@ require("lazy").setup({
     },
 
     -- LSP setup
-    { 'williamboman/mason.nvim' },
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup({})
+        end
+    },
     { 'williamboman/mason-lspconfig.nvim' },
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
-    { 'neovim/nvim-lspconfig' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        config = function()
+            -- LSP config
+            local lsp_zero = require('lsp-zero')
+
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({ buffer = bufnr })
+                vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = bufnr })
+                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+                vim.keymap.set({ 'i' }, "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr })
+                vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+            end)
+        end,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "gopls",
+                    "golangci_lint_ls",
+                    "terraformls",
+                    "tsserver",
+                    "rust_analyzer",
+                    "eslint",
+                    "pyright",
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                },
+            })
+        end,
+    },
     { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
+    {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            local cmp = require('cmp')
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    -- ["<Tab>"] = cmp.mapping.confirm({ select = false }),
+                }),
+                experimental = {
+                    ghost_text = true,
+                }
+            })
+        end
+    },
     { 'L3MON4D3/LuaSnip' },
 
     {
         'stevearc/conform.nvim',
         event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            -- Formatter
+            local conform = require("conform")
+            conform.setup({
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
+                formatters_by_ft = {
+                    go = { "gofumpt", "goimports" },
+                    javascript = { "prettier" },
+                    typescript = { "prettier" },
+                    javascriptreact = { "prettier" },
+                    typescriptreact = { "prettier" },
+                    svelte = { "prettier" },
+                    css = { "prettier" },
+                    html = { "prettier" },
+                    json = { "prettier" },
+                    yaml = { "prettier" },
+                    markdown = { "prettier" },
+                    graphql = { "prettier" },
+                    rust = { "rustfmt" },
+                    python = { "black" },
+                },
+            })
+
+            vim.keymap.set("n", "<leader>fm", function()
+                conform.format({
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                })
+            end)
+        end
     },
 
     {
@@ -86,9 +249,50 @@ require("lazy").setup({
                 width = .7,
             },
         },
+        config = function()
+            -- Zen Mode
+            local zen = require("zen-mode")
+            vim.keymap.set("n", "<leader>z", zen.toggle, {})
+        end,
     },
 
-    { "nvim-tree/nvim-tree.lua" },
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            -- nvim-tree
+            local function nvim_tree_on_attach(bufnr)
+                local api = require("nvim-tree.api")
+
+                -- default mappings
+                api.config.mappings.default_on_attach(bufnr)
+
+                -- custom mappings
+                vim.keymap.set('n', '<leader>b', api.tree.toggle)
+            end
+
+            require("nvim-tree").setup({
+                on_attach = nvim_tree_on_attach,
+                actions = {
+                    open_file = {
+                        quit_on_open = true,
+                    },
+                },
+                renderer = {
+                    icons = {
+                        show = {
+                            file = false,
+                            folder = false,
+                            folder_arrow = false,
+                            git = false,
+                            modified = false,
+                            diagnostics = false,
+                            bookmarks = false,
+                        }
+                    }
+                },
+            })
+        end,
+    },
 
     -- UI select for lsp code action
     { "nvim-telescope/telescope-ui-select.nvim" },
@@ -132,185 +336,3 @@ require("lazy").setup({
         end,
     },
 })
-
--- disable netrw as it's replace by nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-local function nvim_tree_on_attach(bufnr)
-    local api = require("nvim-tree.api")
-
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
-
-    -- custom mappings
-    vim.keymap.set('n', '<leader>b', api.tree.toggle)
-end
-
--- nvim-tree
-require("nvim-tree").setup({
-    on_attach = nvim_tree_on_attach,
-    actions = {
-        open_file = {
-            quit_on_open = true,
-        },
-    },
-    renderer = {
-        icons = {
-            show = {
-                file = false,
-                folder = false,
-                folder_arrow = false,
-                git = false,
-                modified = false,
-                diagnostics = false,
-                bookmarks = false,
-            }
-        }
-    },
-})
-
--- LSP config
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
-    vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { buffer = bufnr })
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
-    vim.keymap.set({ 'i' }, "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr })
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        "lua_ls",
-        "gopls",
-        "golangci_lint_ls",
-        "terraformls",
-        "tsserver",
-        "rust_analyzer",
-        "eslint",
-        "pyright",
-    },
-    handlers = {
-        lsp_zero.default_setup,
-    },
-})
-
-local cmp = require('cmp')
-
-cmp.setup({
-    mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        -- ["<Tab>"] = cmp.mapping.confirm({ select = false }),
-    }),
-    experimental = {
-        ghost_text = true,
-    }
-})
-
--- disable line wrapping
-vim.o.wrap = false
-
-vim.o.termguicolors = true
-
--- Decrease update time
-vim.o.updatetime = 50
-
--- enable numbers and relative numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "100"
-
-vim.g.mapleader = " "
-
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
--- telescope
-local telescope = require('telescope')
-
-telescope.setup {
-    extensions = {
-        ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-        },
-    },
-    pickers = {
-        find_files = {
-            find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
-        }
-    },
-}
-
-telescope.load_extension("ui-select")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fc', builtin.commands, {})
--- telescope fzf
-require('telescope').load_extension('fzf')
-
--- Zen Mode
-local zen = require("zen-mode")
-
-vim.keymap.set("n", "<leader>z", zen.toggle, {})
-
--- Formatter
-local conform = require("conform")
-conform.setup({
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
-    formatters_by_ft = {
-        go = { "gofumpt", "goimports" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        rust = { "rustfmt" },
-        python = { "black" },
-    },
-})
-
-vim.keymap.set("n", "<leader>fm", function()
-    conform.format({
-        timeout_ms = 500,
-        lsp_fallback = true,
-    })
-end)

0a2ae84247b5fa9da2e628f956d9868493c54a25
loikg <mail@company.com>
2024-03-18 19:07:34 +0800

    feat(nvim): add git worktree plugin

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -15,5 +15,19 @@ return {
                 },
             })
         end,
+    },
+    {
+        'ThePrimeagen/git-worktree.nvim',
+        dependencies = {
+            { 'nvim-lua/plenary.nvim' },
+            { 'nvim-telescope/telescope.nvim' },
+        },
+        config = function()
+            require('git-worktree').setup({})
+            local telescope = require('telescope')
+            telescope.load_extension("git_worktree")
+            vim.keymap.set('n', '<leader>ws', telescope.extensions.git_worktree.git_worktrees, {})
+            vim.keymap.set('n', '<leader>ww', telescope.extensions.git_worktree.create_git_worktree, {})
+        end,
     }
 }

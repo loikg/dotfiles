2a9b7f33398966f3253cc621d1da5b284af6afbe
loikg <mail@company.com>
2024-03-18 19:08:47 +0800

    feat(nvim): enable catppuccin theme integration

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -4,5 +4,26 @@ return {
     priority = 1000,
     config = function()
         vim.cmd.colorscheme "catppuccin-latte"
+        require("catppuccin").setup({
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                mason = true,
+                dap = true,
+                dap_ui = true,
+                telescope = {
+                    enabled = true,
+                },
+                lsp_trouble = false,
+            }
+        })
+
+        -- DAP integration config
+        local sign = vim.fn.sign_define
+        sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+        sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+        sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
     end
 }

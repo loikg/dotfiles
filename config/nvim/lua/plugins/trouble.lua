be2e7a56a6d5e8dbd6fb5d885d8bd35a82e70abe
loikg <mail@company.com>
2024-10-19 23:54:06 +0800

    fix(nvim): update trouble config to make it work again

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -6,21 +6,11 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     },
-    config = function()
-        require("trouble").setup({
-            icons = false,
-        })
-
-        vim.keymap.set("n", "<leader>tt", function()
-            require("trouble").toggle()
-        end)
-
-        vim.keymap.set("n", "[t", function()
-            require("trouble").next({ skip_groups = true, jump = true });
-        end)
-
-        vim.keymap.set("n", "]t", function()
-            require("trouble").previous({ skip_groups = true, jump = true });
-        end)
-    end
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+    },
 }

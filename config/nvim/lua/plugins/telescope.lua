1a10616513cce0955a49f4640edef73b58cb9852
loikg <mail@company.com>
2024-11-15 10:38:50 +0800

    feat: let find_files find .env files

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
                 },
                 pickers = {
                     find_files = {
-                        find_command = { 'rg', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules' }
+                        find_command = { 'rg', '--files', '--hidden', '-g', '!.git', '-g', '*.env' }
                     }
                 },
             }
@@ -32,7 +32,6 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fm', builtin.marks, {})
             vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
             vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})

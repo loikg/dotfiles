2d453e420d18c26cd96f98ee53d7e08f6bbda9df
loikg <mail@company.com>
2025-03-24 06:18:09 +0800

    feat(nvim): add godoc plugin

---
+++ config/nvim/lua/plugins/go.lua
@@ -0,0 +1,16 @@
+return {
+    "fredrikaverpil/godoc.nvim",
+    version = "*",
+    dependencies = {
+        { "nvim-telescope/telescope.nvim" },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            opts = {
+                ensure_installed = { "go" },
+            },
+        },
+    },
+    build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+    cmd = { "GoDoc" },                                                 -- optional
+    opts = {},                                                         -- see further down below for configuration
+}

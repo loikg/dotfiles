b2c7c08dcc463634b033707f62e0d9846af6bd3c
loikg <mail@company.com>
2025-05-26 21:57:25 +0800

    feat(ghostty|kitty|alacritty): switch cyberdream theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,29 +1,43 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin-latte"
-        require("catppuccin").setup({
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                treesitter = true,
-                mason = true,
-                dap = true,
-                dap_ui = true,
-                telescope = {
-                    enabled = true,
-                },
-                lsp_trouble = false,
-            }
-        })
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        enabled = false,
+        config = function()
+            vim.cmd.colorscheme "catppuccin-latte"
+            require("catppuccin").setup({
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    mason = true,
+                    dap = true,
+                    dap_ui = true,
+                    telescope = {
+                        enabled = true,
+                    },
+                    lsp_trouble = false,
+                }
+            })
 
-        -- DAP integration config
-        local sign = vim.fn.sign_define
-        sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
-        sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
-        sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
-    end
+            -- DAP integration config
+            local sign = vim.fn.sign_define
+            sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+            sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+            sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+        end
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("cyberdream").setup({
+                variant = "auto",
+            })
+            vim.cmd.colorscheme("cyberdream")
+        end
+    }
 }

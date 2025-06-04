40d0d25583b21421940a05de7aa5deef8cafdcfa
loikg <mail@company.com>
2025-06-04 18:24:00 +0800

    feat(nvim): configure avante and copilot

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,11 +1,11 @@
 return {
     "yetone/avante.nvim",
-    enabled = false,
     event = "VeryLazy",
-    lazy = false,
-    version = "v0.0.9", -- set this if you want to always pull the latest change
+    version = false, -- Never set this value to "*"! Never!
     opts = {
         -- add any opts here
+        -- for example
+        provider = "copilot",
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
@@ -16,25 +16,35 @@ return {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",      -- for providers='copilot'
-        -- {
-        --     -- support for image pasting
-        --     "HakonHarnes/img-clip.nvim",
-        --     event = "VeryLazy",
-        --     opts = {
-        --         -- recommended settings
-        --         default = {
-        --             embed_image_as_base64 = false,
-        --             prompt_for_file_name = false,
-        --             drag_and_drop = {
-        --                 insert_mode = true,
-        --             },
-        --             -- required for Windows users
-        --             use_absolute_path = true,
-        --         },
-        --     },
-        -- },
+        -- "echasnovski/mini.pick",     -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+        {
+            "zbirenbaum/copilot.lua",
+            config = function()
+                require('copilot').setup({
+                    suggestion = { enabled = false },
+                    panel = { enabled = false },
+                })
+            end
+        }, -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
         {
             -- Make sure to set this up properly if you have lazy=true
             'MeanderingProgrammer/render-markdown.nvim',
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,10 @@ return {
     {
         'saghen/blink.cmp',
         version = '1.*',
+        dependencies = {
+            'Kaiser-Yang/blink-cmp-avante',
+            'giuxtaposition/blink-cmp-copilot',
+        },
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
@@ -37,13 +41,26 @@ return {
             },
 
             sources = {
-                default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'avante', 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
                 providers = {
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
                         -- make lazydev completions top priority (see `:h blink.cmp`)
                         score_offset = 100,
+                    },
+                    avante = {
+                        module = 'blink-cmp-avante',
+                        name = 'Avante',
+                        opts = {
+                            -- options for blink-cmp-avante
+                        }
+                    },
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                        score_offset = 100,
+                        async = true,
                     },
                 },
             },

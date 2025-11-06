8d42fc9db84cd28c5876c3a11076440196dcf03f
loikg <mail@company.com>
2025-11-06 11:50:44 +0800

    feat(sqlite): add sqliterc

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ installOnLinux() {
     stow wezterm
     stow ghostty
     stow kitty
+    stow sqlite
 }
 
 installOnMacos() {
@@ -32,6 +33,7 @@ installOnMacos() {
     stow wezterm
     stow ghostty
     stow kitty
+    stow sqlite
 }
 
 

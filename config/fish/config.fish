beb902abbca16955c1315cd9b0ba0ee97d69e685
loikg <mail@company.com>
2026-03-29 10:09:22 +0800

    feat(mise): configure mise in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -52,3 +52,6 @@ if test (uname) = Darwin
     # Configure 1password ssh agent for macOS
     set -gx SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 end
+
+# mise-en-place
+mise activate fish | source
